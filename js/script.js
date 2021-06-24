97de3b2350cd0ab0a41b95547e44bbb8315fdbfc
Osvaldss <zadeikisosvaldas@gmail.com>
2021-06-24 17:28:33 +0000

    Add script tag into html head

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let buttons = document.getElementsByTagName('button');
 
     for (let button of buttons){
-        button.addEventListener('click', function(){
+        button.addEventListener('click', function() {
             if (this.getAttribute('data-type') === 'submit'){
                alert('You clicked Submit!');
             } else {

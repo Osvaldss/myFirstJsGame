04b9938cd032822736fb681329c4927706312668
Osvaldss <zadeikisosvaldas@gmail.com>
2021-06-26 12:59:01 +0000

    Added multiply game function to work

--- js/script.js
+++ js/script.js
@@ -20,7 +20,9 @@ function runGame(gameType) {
 
     if (gameType === 'addition'){
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if(gameType === 'multiply') {
+        displayMultiplyQuestion(num1, num2);
+    }else {
         alert(`Uknown game type ${gameType}`);
         throw `Uknown game type ${gameType}, aborting!`
     }
@@ -33,8 +35,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert('Hey! You got it right! :D');
+        incrementScore();
     } else {
         alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculateAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     runGame(calculateAnswer[1]);
 }
@@ -46,18 +50,22 @@ function calculateCorrectAnswer() {
 
     if (operator === '+'){
         return [operand1 + operand2, 'addition'];
-    } else {
+    } else if (operator === 'x') {
+        return [operand1 * operand2, 'multiply'];
+    }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}, aborting!`;
     }
 }
 
 function incrementScore() {
-
+    let oldscore = parseInt(document.getElementById('correct').innerText);
+    document.getElementById('correct').innerText = ++oldscore;
 }
 
 function incrementWrongAnswer() {
-
+    let oldscore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldscore; 
 }
 
 function displayAdditionQuestion(operand1, operand2) {
@@ -71,8 +79,10 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = 'x';
 }
 
 // function displayDivideQuestion(){} 
